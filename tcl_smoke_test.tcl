25f468697dd72f2f3b7c1186a689821720ddec27
Samuel Wilson <samWson@users.noreply.github.com>
2023-11-17 14:41:08 +1300

    Show the Tcl version and the path of the interpreter running the script

--- tcl_smoke_test.tcl
+++ tcl_smoke_test.tcl
@@ -1,12 +1,16 @@
 #!/usr/bin/env wish
 
-# Create a simple window with a button to confirm Tk is installed
-wm title . "Tk Checker"
-label .label -text "Looks like Tk is installed! Woohoo!"
+set tclVersion [info tclversion]
+set interpreterPath [info nameofexecutable]
+
+wm title . "Tcl/Tk Inspector"
+label .versionLabel -text "Tcl version: $tclVersion"
+label .pathLabel -text "Interpreter location: $interpreterPath"
 button .quit -text "Quit" -command { exit }
 
 # Pack the widgets
-pack .label -padx 10 -pady 10
+pack .versionLabel -padx 10 -pady 5
+pack .pathLabel -padx 10 -pady 5
 pack .quit -padx 10 -pady 10
 
 # Start the Tk event loop
