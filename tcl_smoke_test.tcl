eee954599ac9d046801e1f000131bbdb9f3d8b66
Samuel Wilson <samWson@users.noreply.github.com>
2023-11-17 15:05:29 +1300

    Show path to Tcl installation

--- tcl_smoke_test.tcl
+++ tcl_smoke_test.tcl
@@ -1,16 +1,19 @@
 #!/usr/bin/env wish
 
 set tclVersion [info tclversion]
+set tclInstallationPath [info library]
 set interpreterPath [info nameofexecutable]
 
 wm title . "Tcl/Tk Inspector"
 label .versionLabel -text "Tcl version: $tclVersion"
-label .pathLabel -text "Interpreter location: $interpreterPath"
+label .runningInterpreterLabel -text "Currently executing interpreter: $interpreterPath"
+label .installPathLabel -text "Tcl installation path: $tclInstallationPath"
 button .quit -text "Quit" -command { exit }
 
 # Pack the widgets
 pack .versionLabel -padx 10 -pady 5
-pack .pathLabel -padx 10 -pady 5
+pack .runningInterpreterLabel -padx 10 -pady 5
+pack .installPathLabel -padx 10 -pady 5
 pack .quit -padx 10 -pady 10
 
 # Start the Tk event loop
